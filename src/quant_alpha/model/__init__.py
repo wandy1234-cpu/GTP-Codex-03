f5a42b58f173bf981d1e7e1f782ef9635f1ed744
wandy1234-cpu <wandy1234@gmail.com>
2026-04-11 20:58:49 +0800

    fix: add missing walk_forward module used by daily pipeline

--- src/quant_alpha/model/__init__.py
+++ src/quant_alpha/model/__init__.py
@@ -1,3 +1,4 @@
 from quant_alpha.model.ranker import top_n_latest, train_ranker
+from quant_alpha.model.walk_forward import build_walk_forward_windows, fold_metrics
 
-__all__ = ["train_ranker", "top_n_latest"]
+__all__ = ["train_ranker", "top_n_latest", "build_walk_forward_windows", "fold_metrics"]
